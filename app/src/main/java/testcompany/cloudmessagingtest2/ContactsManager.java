07da9d16006c54d36dfff5b5ea1b4e3f1305c944
haukurb <haukzi@gmail.com>
2017-03-04 16:54:53 +0000

    Beginning of ContactsDatabaseHelper and ContactsManager

---
+++ app/src/main/java/testcompany/cloudmessagingtest2/ContactsManager.java
@@ -0,0 +1,10 @@
+package testcompany.cloudmessagingtest2;
+
+/**
+ * Created by Notandi on 04-Mar-17.
+ */
+
+public class ContactsManager {
+//    AuthHelper mAuthHelper;
+//    ContactsDatabaseHelper
+}
