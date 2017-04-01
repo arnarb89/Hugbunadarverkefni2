7201598a276a82753eaa512598c86bd69ba066a2
arnarb89 <arnarb89@gmail.com>
2017-04-01 19:03:38 +0000

    Fixed AddContact

--- app/src/main/java/testcompany/cloudmessagingtest2/NewFriendsActivity.java
+++ app/src/main/java/testcompany/cloudmessagingtest2/NewFriendsActivity.java
@@ -103,6 +103,7 @@ public class NewFriendsActivity extends Activity {
                                             final String username = response.getString("username");
                                             Log.i("testing", "userId: " + userId + " username: " + username);
 
+
 //                            final int userId_final = userId;
 //                            final String username_final = username;
 
@@ -111,7 +112,7 @@ public class NewFriendsActivity extends Activity {
                                                 @Override
                                                 public void onClick(View v) {
                                                     //contactsManager.addContact(new Contact(userId, username, false));
-                                                    contactsManager.storeRequest(new Contact(userId, username, false));
+                                                    contactsManager.sendFriendRequest(new Contact(userId, username, false));
                                                     Toast.makeText(NewFriendsActivity.this, "Contact added.", Toast.LENGTH_SHORT).show();
                                                 }
                                             });
